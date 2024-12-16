97631d4a54b9fe8fc97f0c35a31d9f4e48644c4c
Alessio Lodato <alss.ldt@gmail.com>
2024-12-16 19:12:24 +0000

    set debug to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,8 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 ALLOWED_HOSTS = ["8000-alelodato-djangoblog-9qbcclalo9o.ws-eu117.gitpod.io",
 ".herokuapp.com"
 ]
